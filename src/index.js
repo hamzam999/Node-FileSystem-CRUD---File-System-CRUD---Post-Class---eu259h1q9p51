5d0ba797317003279844f7da481ebb80eab5671c
hamzam999 <hamza.m999@gmail.com>
2022-10-12 02:07:01 +0530

    CRUD

--- src/index.js
+++ src/index.js
@@ -1,26 +1,37 @@
 const fs = require('fs/promises')
 
 const myFileWriter = async (fileName, fileContent) => {
-	// write code here
-	// dont chnage function name
+  try {
+    await fs.writeFile(`${fileName}`, `${fileContent}`)
+  } catch (err) {
+    console.log(err)
+  }
 }
 
 const myFileReader = async (fileName) => {
-	// write code here
-	// dont chnage function name
+  try {
+    const data = await fs.readFile(`${fileName}`, { encoding: 'utf8' })
+    console.log(data)
+  } catch (err) {
+    console.log(err)
+  }
 }
 
-
 const myFileUpdater = async (fileName, fileContent) => {
-	// write code here
-	// dont chnage function name
+  try {
+    await fs.appendFile(`${fileName}`, `${fileContent}`)
+  } catch (err) {
+    console.log(err)
+  }
 }
 
 const myFileDeleter = async (fileName) => {
-	// write code here
-	// dont chnage function name
+  try {
+    await fs.unlink(`${fileName}`)
+  } catch (err) {
+    console.log(err)
+  }
 }
 
-
-
 module.exports = { myFileWriter, myFileUpdater, myFileReader, myFileDeleter }
+ 
